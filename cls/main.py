632b7497e2b4a14bb73641503aa61f9c3ce8d888
Kevin Baker <kevinbaker42@gmail.com>
2023-03-07 22:52:42 -0600

    use rofimenu

--- cls/main.py
+++ cls/main.py
@@ -2,51 +2,56 @@
 
 import subprocess
 from pathlib import Path
+from rofi_menu import Menu, Back, run_menu
+
 
 # Set the root directory to search and define the Rofi theme to use
 root_dir = Path("~/code").expanduser()
 rofi_theme = Path("~/code/themes/redblack.rasi").expanduser()
 
-# Get a list of all subdirectories in the root directory that are not hidden, have subdirectories,
-# and have at least one file or symlink in them, and do not contain .git or __pycache__
-all_subdirs = [
-    subdir for subdir in root_dir.glob("**/*")
-    if subdir.is_dir() and not any(name in str(subdir) for name in [".git", "__pycache__"])
-]
+# Define the directory menu
+def directory_menu():
+    # Get a list of all subdirectories in the root directory that are not hidden, have subdirectories,
+    # and have at least one file or symlink in them, and do not contain .git or __pycache__
+    all_subdirs = [
+        subdir for subdir in root_dir.glob("**/*")
+        if subdir.is_dir() and not any(name in str(subdir) for name in [".git", "__pycache__"])
+    ]
 
-# Sort the list of directories alphabetically and use rofi to display a menu of them
-selected_dir = subprocess.check_output(
-    ["rofi", "-dmenu", "-p", "Select directory:", "-format", "s", "-theme", str(rofi_theme)],
-    input="\n".join(sorted(str(subdir) for subdir in all_subdirs)),
-    text=True,
-).strip()
+    # Sort the list of directories alphabetically and use rofi to display a menu of them
+    return Menu(options=sorted(str(subdir) for subdir in all_subdirs), prompt="Select directory:")
 
-# Exit gracefully if no directory is selected
-if not selected_dir:
-    exit()
 
-# Use rofi to display a menu of the applications to open the selected directory in
-selected_app = subprocess.check_output(
-    [
-        "rofi",
-        "-dmenu",
-        "-p",
-        "Select application:",
-        "-format",
-        "s",
-        "-theme",
-        str(rofi_theme),
-    ],
-    input="Visual Studio Code\nNemo\nAlacritty",
-    text=True,
-).strip()
-
-# Open the selected directory in the selected application
-if selected_app == "Visual Studio Code":
-    subprocess.Popen(["code", "-n", selected_dir])
-elif selected_app == "Nemo":
-    subprocess.Popen(
-        ["nemo", selected_dir], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
+# Define the application menu
+def application_menu(selected_dir):
+    # Use rofi to display a menu of the applications to open the selected directory in
+    return Menu(
+        options=["Visual Studio Code", "Nemo", "Alacritty"],
+        prompt=f"Select application to open {selected_dir} in:",
     )
-elif selected_app == "Alacritty":
-    subprocess.Popen(["alacritty", "-e", "cd", selected_dir])
+
+
+# Define the main function to run the menus
+def main():
+    # Run the directory menu
+    selected_dir = run_menu(directory_menu(), rofi_args=["-theme", str(rofi_theme)])
+    if not selected_dir:
+        return
+
+    # Run the application menu
+    selected_app = run_menu(application_menu(selected_dir), rofi_args=["-theme", str(rofi_theme)])
+    if not selected_app:
+        return
+
+    # Open the selected directory in the selected application
+    if selected_app == "Visual Studio Code":
+        subprocess.Popen(["code", "-n", selected_dir])
+    elif selected_app == "Nemo":
+        subprocess.Popen(
+            ["nemo", selected_dir], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
+        )
+    elif selected_app == "Alacritty":
+        subprocess.Popen(["alacritty", "-e", "cd", selected_dir])
+
+if __name__ == "__main__":
+    main()
