93043488be142df1b2c69b28f55222a44ea6e4f0
Kevin Baker <kevinbaker42@gmail.com>
2023-03-07 22:48:51 -0600

    refactor

--- cls/main.py
+++ cls/main.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3.10
 
-import os
 import subprocess
-import sys
 from pathlib import Path
 
 # Set the root directory to search and define the Rofi theme to use
@@ -12,20 +10,20 @@ rofi_theme = Path("~/code/themes/redblack.rasi").expanduser()
 # Get a list of all subdirectories in the root directory that are not hidden, have subdirectories,
 # and have at least one file or symlink in them, and do not contain .git or __pycache__
 all_subdirs = [
-    str(subdir) for subdir in root_dir.glob("**/*")
+    subdir for subdir in root_dir.glob("**/*")
     if subdir.is_dir() and not any(name in str(subdir) for name in [".git", "__pycache__"])
 ]
 
 # Sort the list of directories alphabetically and use rofi to display a menu of them
 selected_dir = subprocess.check_output(
     ["rofi", "-dmenu", "-p", "Select directory:", "-format", "s", "-theme", str(rofi_theme)],
-    input="\n".join(sorted(all_subdirs)),
+    input="\n".join(sorted(str(subdir) for subdir in all_subdirs)),
     text=True,
 ).strip()
 
 # Exit gracefully if no directory is selected
 if not selected_dir:
-    sys.exit()
+    exit()
 
 # Use rofi to display a menu of the applications to open the selected directory in
 selected_app = subprocess.check_output(
